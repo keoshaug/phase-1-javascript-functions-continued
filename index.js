0c7379712f94987aea35318bf04cbc4cdd135644
Kevin OShaughnessy <kevinoshaughnessy@ymail.com>
2023-03-24 21:14:59 -0400

    functionscont

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(something= 'roller-skate') {
+    
+    return `This Saturday, I want to ${something}!`
+}
+
+function mondayWork(something = `go to the office`) {
+    return `This Monday, I will ${something}.`
+}
+
+wrapAdjective("||")("||a dedicated programmer||")
+
+function wrapAdjective(high = "*") {
+    return function (value = "special") {
+        return `You are ${high}${value}${high}!`
+    }
+    
+}
